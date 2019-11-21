0aef88d3ee0a1669e98be32221b498d3e79ff658
manuelwecker <manuelwecker@web.de>
2019-11-21 11:39:44 +0100

    Import google font

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -11,9 +11,7 @@ export default function GlobalStyles() {
           text-align: center;
           margin: 0px;
           padding: 0px;
-          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-            "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-            "Helvetica Neue", sans-serif;
+          font-family: -apple-system, BlinkMacSystemFont, "Alata", sans-serif;
           -webkit-font-smoothing: antialiased;
           -moz-osx-font-smoothing: grayscale;
         }
