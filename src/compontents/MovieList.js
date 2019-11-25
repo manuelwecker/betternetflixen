1a82b21b4ce9b07cf927e0a8ece7a2338acbe969
manuelwecker <manuelwecker@web.de>
2019-11-25 11:04:22 +0100

    Add now with API build

--- src/compontents/MovieList.js
+++ src/compontents/MovieList.js
@@ -3,11 +3,14 @@ import { getDiscoverMovies } from "../api/movies";
 
 export function MovieList() {
   const [movies, setMovies] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
 
   console.log(movies);
 
   async function refreshMovies() {
+    setLoading(true);
     const discoveredMovies = await getDiscoverMovies();
+    setLoading(false);
     setMovies(discoveredMovies);
   }
   // Komponente wird aufgerufen einmalig, oder wenn sich die Property des Arrays
@@ -15,5 +18,21 @@ export function MovieList() {
     refreshMovies();
   }, []);
 
-  return <div>MovieList</div>;
+  // <button onClick={refreshMovies}></button>
+
+  return (
+    <div>
+      {loading && <div>Loading</div>}
+      {movies.map(movie => (
+        <div key={movie.id}>
+          {movie.title}
+          <img
+            key={movie.id}
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
